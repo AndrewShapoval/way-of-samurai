c0fbbfae8657f43b37c54041239ca811246c83e9
AndrewShapoval <andrewshapoval88@gmail.com>
2020-12-06 22:03:07 +0300

    refactor paginator (93)

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,23 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Paginator.module.css";
 
-export let Paginator = (props: PropsUsersType) => {
+export let Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged, portionSize = 20}: PropsUsersType) => {
 
-    let pageCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    let pageCount = Math.ceil(totalUsersCount / pageSize)
 
     let pages = []
     for (let i = 1; i <= pageCount; i++) {
         pages.push(i)
     }
 
+    let portionCount = Math.ceil(pageCount / portionSize)
+    let [portionNumber, setPortionNumber] = useState(1)
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+    let rightPortionPageNumber = portionNumber * portionSize
+    debugger
+
     return (
-        <div>
-            {pages.map(p => {
-                return <span className={props.currentPage === p ? styles.selectedPage : ""}
-                             onClick={() => {
-                                 props.onPageChanged(p)
-                             }}>{p}</span>
-            })}
+        <div className={styles.paginator}>
+            <button onClick={() => {
+                setPortionNumber(portionNumber - 1)
+            }}
+                    disabled={portionNumber === 1}>Prev
+            </button>
+
+            {pages
+                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                .map(p => {
+                    return <span className={currentPage === p ? styles.selectedPage : styles.pageNumber}
+                                 onClick={() => {
+                                     onPageChanged(p)
+                                 }}>{p} </span>
+                })}
+            <button onClick={() => {
+                setPortionNumber(portionNumber + 1)
+            }}
+                    disabled={portionCount === portionNumber}>Next
+            </button>
         </div>
     )
 }
@@ -27,4 +46,6 @@ type PropsUsersType = {
     pageSize: number
     currentPage: number
     onPageChanged: (pageNumber: number) => void
+    portionSize?: number
+
 }
